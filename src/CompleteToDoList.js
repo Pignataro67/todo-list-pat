6c3766177e226654b1fddee4e6b0d3f3317b7490
Pignataro67 <Pignataro67@gmail.com>
2019-02-01 04:46:24 -0800

    adding filter and sort func

---
+++ src/CompleteToDoList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class CompleteToDoList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.props.todos.map(todo => {
+            return <p key={todo.name}>
+            <input type='checkbox' checked onChange={() => this.props.change(todo.id)} />
+            {todo.name}
+            </p>
+          })}
+        </h1>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  const filteredCompleteTodo = state.filter(todo => todo.completed)
+    return {todos: filteredCompleteTodo}
+}
+
+export default connect(mapStateToProps)(CompleteToDoList);
---
+++ src/NamesStartWithA.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// class NamesStartWithA extends React.Component {
+//   render() {
+//     return (
+//       <h1>
+//         {this.props.names.map(name => {
+//           return <p key={name}>{name}</p>
+//         })}
+//       </h1>
+//     )
+//   }
+// }
+
+// export default NamesStartWithA
